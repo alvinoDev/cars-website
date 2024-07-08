a7fe8ff0e48ee2ff64e3434a4a3f2b392153e682
alvinoDev <48383924+alvinoDev@users.noreply.github.com>
2024-07-08 17:01:46 -0400

    Feature: Add Header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,43 @@
 /* ===================== HEADER ===================== */
 
+const navMenu = document.getElementById('nav_menu'),
+	navToggle = document.getElementById('nav_toggle'),
+	navClose = document.getElementById('nav_close');
+
 // SHOW MENU
+if (navToggle) {
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show_menu');
+	});
+}
+
+// HIDDEN MENU
+if (navClose) {
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show_menu');
+	});
+}
 
 // REMOVE MENU MOBILE
+const navLink = document.querySelectorAll('.nav_link');
+
+function linkAction() {
+	const navMenu = document.getElementById('nav_menu');
+	navMenu.classList.remove('show_menu');
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 // CHANGE BACKGROUND HEADER
 
 /* ====================== MAIN ====================== */
 
-
 // SECTION - POPULAR
 
 // SWIPER
 
-
 // SECTION - FEATURED
 
 // MIXITUP FILTER
-
 
 // Link active products
 
