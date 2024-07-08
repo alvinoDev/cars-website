48dee45684917489575a661ded839504d4a3067b
alvinoDev <48383924+alvinoDev@users.noreply.github.com>
2024-07-08 15:42:33 -0400

    Feature: Configuracion inicial del proyecto

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/* ===================== HEADER ===================== */
+
+// SHOW MENU
+
+// REMOVE MENU MOBILE
+
+// CHANGE BACKGROUND HEADER
+
+/* ====================== MAIN ====================== */
+
+
+// SECTION - POPULAR
+
+// SWIPER
+
+
+// SECTION - FEATURED
+
+// MIXITUP FILTER
+
+
+// Link active products
+
+/* ================= SCROLL ACTIONS ================= */
+
+// SHOW SCROLL UP
+
+// SCROLL SECTIONS ACTIVE LINK
+
+// SCROLL REVEAL ANIMATION
